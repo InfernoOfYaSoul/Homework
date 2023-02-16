da5bdee15bf9da55ac754fc2ef0690f3f28bc199
InfernoOfYaSoul <dmitrii190520@mail.ru>
2023-02-16 09:16:21 +0300

    changed B

--- week01/B.cpp
+++ week01/B.cpp
@@ -7,6 +7,7 @@ int main(){
     int a, b, c, D;
     cin >> a >> b >> c;
     D = b * b - 4 * a * c;
+    int ans1 = 0, ans2 = 0;
     if(D < 0){
         cout << "\n" << endl << endl;
     }
@@ -14,6 +15,12 @@ int main(){
         cout << (-b) / (2 * a) << endl;
     }
     if(D > 0){
-        cout << (-b + sqrt(D)) / (2 * a) << " " <<(-b - sqrt(D)) / (2 * a) << endl;
+        ans1 = (-b + sqrt(D)) / (2 * a);
+        ans2 = (-b - sqrt(D)) / (2 * a);
+        if(ans1 >= ans2){
+            cout << ans2 << " " << ans1 << endl;
+        }else{
+            cout << ans1 << " " << ans2 << endl;
+        }
     }
 }
