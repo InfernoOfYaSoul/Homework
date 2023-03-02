5df7fef41940eb3d8af7fa89885bb1e08a6630e9
InfernoOfYaSoul <dmitrii190520@mail.ru>
2023-03-02 11:50:30 +0300

    D upd

--- week03/D.cpp
+++ week03/D.cpp
@@ -12,14 +12,13 @@ void swap(int& lha, int& rha){
 }
 
 int main() {
-    int a[N], n;
-    cin >> n;
+    int a[N];
     for(int i = 0; i < N; i++){
         cin >> a[i];
     }
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < N - i -1; j++){
-            if (a[j] * a[j + 1] < 0){
+        for(int j = 0; j < N - i - 1; j++){
+            if (a[j] < 0 &  a[j + 1] > 0){
                 swap(a[j], a[j+1]);
             }
         }
